c1f06a8f6856a6449a07ad4f249a3b320c753054
Laxmikant Pandhare <lpandhar@redhat.com>
2020-08-27 11:50:00 -0700

    added example operator

---
+++ examples/memcached-operator/src/main/java/io/fabric8/memcached/operator/memcached_types/MemcachedList.java
@@ -0,0 +1,6 @@
+package io.fabric8.memcached.operator.memcached_types;
+
+import io.fabric8.kubernetes.client.CustomResourceList;
+
+public class MemcachedList extends CustomResourceList<Memcached> {
+}
---
+++ examples/memcached-operator/src/main/java/io/fabric8/memcached/operator/memcached_types/MemcachedSpec.java
@@ -0,0 +1,24 @@
+package io.fabric8.memcached.operator.memcached_types;
+
+public class MemcachedSpec {
+
+    private int Size;
+
+    /**
+     * Gets Size.
+     *
+     * @return the get Size
+     */
+    public int getSize() {
+        return Size;
+    }
+
+    /**
+     * Sets Size.
+     *
+     * @param size the spec
+     */
+    public void setSize(int size) {
+        Size = size;
+    }
+}
