6e441f4d025e08e0a463b4c89cea64565ba2f487
Laxmikant Pandhare <lpandhar@redhat.com>
2020-10-08 23:06:18 -0700

    fixed issue with memcached java sample operator

--- examples/memcached-operator/src/main/java/io/fabric8/memcached/operator/memcached_types/MemcachedSpec.java
+++ examples/memcached-operator/src/main/java/io/fabric8/memcached/operator/memcached_types/MemcachedSpec.java
@@ -1,24 +1,38 @@
+// Copyright 2020 The Operator-SDK Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package io.fabric8.memcached.operator.memcached_types;
 
 public class MemcachedSpec {
 
-    private int Size;
+    private int size;
 
     /**
-     * Gets Size.
+     * Gets size.
      *
-     * @return the get Size
+     * @return the get size
      */
     public int getSize() {
-        return Size;
+        return size;
     }
 
     /**
-     * Sets Size.
+     * Sets size.
      *
      * @param size the spec
      */
     public void setSize(int size) {
-        Size = size;
+        this.size = size;
     }
 }
